c073dd6962618a2a77012503edca550bc9886634
Joey <joey@xm8.ninja>
2020-07-18 13:19:11 -0400

    Created multiple products within `redux store` and combined them together using `rootReducer`.

--- scoops/src/App.js
+++ scoops/src/App.js
@@ -1,29 +1,25 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Loader from "react-loaders";
-// import "./App.scss";
-
-let loader = <Loader type="square-spin" />;
+import CakeContainer from "./Components/CakeContainer";
+import IceCreamContainer from "./Components/IceCreamContainer";
+import { Provider } from "react-redux";
+import store from "./Redux/store";
+import HooksCakeContainer from "./Components/HooksCakeContainer";
+import HooksMilkShakeContainer from "./Components/HooksMilkShakeContainer";
+import NewCakeContainer from "./Components/NewCakeContainer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <HooksCakeContainer />
+        <CakeContainer />
+        <IceCreamContainer />
+        <HooksMilkShakeContainer />
+        <NewCakeContainer />
+      </div>
+    </Provider>
   );
 }
 
--- scoops/src/Redux/store.js
+++ scoops/src/Redux/store.js
@@ -1,6 +1,12 @@
-// #7 @ 3:26
+import { createStore, applyMiddleware } from "redux";
+import cakeReducer from "./cake/cakeReducer";
+import rootReducer from "./rootReducer";
+import logger from "redux-logger";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-import redux from "redux";
-import { reducer } from "../Reducers/reducers";
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger))
+);
 
-const store = createStore(reducer);
+export default store;
